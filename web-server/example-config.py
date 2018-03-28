5c540b57f03caf80b6808f4ea4de77e883cadb16
Sam Kreter <samkreter@gmail.com>
2018-03-27 21:43:52 -0700

    adding example configurations for formatting

---
+++ web-server/example-config.py
@@ -0,0 +1,13 @@
+
+
+
+#Cosmosdb mongodb api connection string
+DATABASE_URI = "" 
+
+#Service Bus Queue Config
+queueConf = {
+      'service_namespace': '',
+      'saskey_name': '',
+      'saskey_value': '',
+      'queue_name': ''
+}
